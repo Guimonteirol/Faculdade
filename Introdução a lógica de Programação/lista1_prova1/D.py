813f06fd811024fa18f03f302ee86bc984c6daed
Guilherme Monteiro <86750421+Guimonteirol@users.noreply.github.com>
2021-09-28 17:57:35 -0300

    Add files via upload

---
+++ Introdução a lógica de Programação/lista1_prova1/D.py
@@ -0,0 +1,9 @@
+K, J, H, A, B = input().split()
+K = int(K)
+J = int(J)
+H = int(H)
+A = int(A)
+B = int(B)
+
+T = K + J + H + A + B
+print(T)
---
+++ Introdução a lógica de Programação/lista1_prova1/E.py
@@ -0,0 +1,5 @@
+N = int(input())
+u = N//1 % 10
+d = N//10 % 10
+c = N//100
+print(u, d, c)
